b2427a2afda4ac9733e10e60159229b8653ae684
an-ivannikov <info@ivannikov.pro>
2022-10-31 11:01:56 +0400

    Update p202-mainnet_config.go

--- config/params/p202-mainnet_config.go
+++ config/params/p202-mainnet_config.go
@@ -53,7 +53,7 @@ func P202MainnetConfig() *BeaconChainConfig {
 	cfg.ShardingForkVersion = []byte{0x04, 0x00, 0x02, 0x02}
 
 	// Bellatrix
-	cfg.TerminalTotalDifficulty = "20000"
+	cfg.TerminalTotalDifficulty = "25000"
 
 	cfg.InitializeForkSchedule()
 	return cfg
